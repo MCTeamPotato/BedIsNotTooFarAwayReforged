f59214e61efdecf80cbd41b689fd539d902fdd21
Kasualix <670067575@qq.com>
2023-10-05 12:41:10 +0800

    port

--- src/main/java/com/teampotato/bintfar/mixin/ServerPlayerMixin.java
+++ src/main/java/com/teampotato/bintfar/mixin/ServerPlayerMixin.java
@@ -1,14 +1,14 @@
 package com.teampotato.bintfar.mixin;
 
-import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.server.level.ServerPlayer;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ServerPlayerEntity.class)
-public abstract class ServerPlayerEntityMixin {
+@Mixin(ServerPlayer.class)
+public abstract class ServerPlayerMixin {
     @Inject(method = "bedInRange", at = @At("HEAD"), cancellable = true)
     private void onCheckBed(@NotNull CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(true);
